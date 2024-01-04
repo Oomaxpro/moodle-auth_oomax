9f4544a1ba05fdb145376c85454a6ef1711abb6d
DS <93175394+SwayzeD@users.noreply.github.com>
2024-01-03 21:44:02 -0500

    Add files via upload

---
+++ vendor/version.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file is part of the Lambda Solutions Cognito Authentication package.
+ *
+ * Copyright (c) 2020 Lambda Solutions
+ *
+ * @package     auth_cognito
+ * @author      Bojan Bazdar
+ * @copyright   2022 Lambda Solutions
+ * @license     MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2023102600;             // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '1.2.0.2';
+$plugin->requires  = 2015051100;             // MDL-2.9+
+$plugin->component = 'auth_cognito';         // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = 'MATURITY_STABLE';
