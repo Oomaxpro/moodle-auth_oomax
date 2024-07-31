c95008d5574dbd2d5d6d8d6983697f00ec5fbd95
DS <93175394+SwayzeD@users.noreply.github.com>
2024-07-31 18:32:07 -0400

    Update auth.php
    Update reported sanity check
    
    Signed-off-by: DS <93175394+SwayzeD@users.noreply.github.com>

--- auth.php
+++ auth.php
@@ -50,8 +50,7 @@ class auth_plugin_cognito extends auth_plugin_base {
 
         $plugin = 'cognito';
         $this->authtype = $plugin;
-        if (property_exists($SESSION, 'logout')) {
-            $this->logouturl = $SESSION->logout;
+        if (!is_null($SESSION) && property_exists($SESSION, 'logout')) { $this->logouturl = $SESSION->logout; }
         }
         $this->config = get_config('auth_cognito');
     }
