252b9e3bdbea5e1cf7ca00ec9b595ecf03210ae1
DS <93175394+SwayzeD@users.noreply.github.com>
2024-10-02 13:23:58 -0400

    tidy up

--- lang/en/auth_cognito.php
+++ lang/en/auth_cognito.php
@@ -28,29 +28,25 @@
 
 /** global $CFG; - Variable "$CFG" is not expected in a lang file */
 
-$string['pluginname'] = 'Oomax Pro Authentication';
-$string['auth_cognito_title'] = "Oomax Pro Authentication Plugin";
-$string['auth_cognito_settings'] = "Oomax Pro Authentication Settings";
 $string['auth_cognito_description'] = "An Authentication Plugin by Oomax Pro";
 $string['cognito_settings'] = 'Oomax Pro Settings';
-$string['public_key_settings'] = 'Oomax Pro public key';
+$string['auth_cognito_title'] = "Oomax Pro Authentication Plugin";
+$string['pluginname'] = 'Oomax Pro Authentication';
+$string['auth_cognito_settings'] = "Oomax Pro Authentication Settings";
 $string['cognito_serverless_login_url'] = 'Oomax Pro serverless login url';
+$string['public_key_settings'] = 'Oomax Pro public key';
 $string['config_lock_email'] = 'Lock email address';
 $string['config_lock_email_desc'] = 'When checked users cannot update their email address';
-
+$string['oomax_uri'] = 'Oomax SSO URI';
 $string['sso_bypass'] = 'Enable SSO By-Pass';
 $string['sso_bypass_desc'] = 'This enables SSO forwarding to the OOMAX URL and allows bypassing forwarding, bypass requires <i>sso=1</i> included in paths';
-
-$string['oomax_uri'] = 'Oomax SSO URI';
-$string['oomax_uri_desc'] = 'Oomax Pro Server URI Address';
-
 $string['cachedef_oomax_cache'] = 'Oomax Cache Store';
-
-$string['invalid_token'] = 'User token is invalid.';
+$string['oomax_uri_desc'] = 'Oomax Pro Server URI Address';
 $string['audience_fail_enrol'] = 'Failed to add user to audience {$a->cohortid} : {$a->message}';
+$string['invalid_token'] = 'User token is invalid.';
 $string['course_not_exist'] = 'Course {$a->courseid} does not exist';
-$string['course_user_enrolled'] = 'User is already enrolled in course {$a->courseid}';
 $string['course_failed_enrol'] = 'Failed to enrol user in course {$a->courseid}';
+$string['course_user_enrolled'] = 'User is already enrolled in course {$a->courseid}';
 $string['course_failed_enrol_msg'] = 'Failed to enrol user in course {$a->courseid} : {$a->message}';
 $string['groups_failed_user_group'] = 'Failed to add user to group {$a->groupid}';
 $string['groups_failed_user_group_msg'] = 'Failed to add user to group {$a->groupid} : {$a->message}';
