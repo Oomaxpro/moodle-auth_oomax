bee871278f6fce9c95679c73f736eaa1d701f3e1
Dustin Brisebois <dustin.brisebois@lambdasolutions.net>
2024-11-12 12:13:18 -0800

    remove extra character

--- version.php
+++ version.php
@@ -33,4 +33,3 @@
  $plugin->requires  = 2015051100;             // MDL-2.9+.
  $plugin->component = 'auth_cognito';         // Full name of the plugin (used for diagnostics).
  $plugin->maturity  = 'MATURITY_STABLE';
- 
